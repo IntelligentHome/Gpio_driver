1fae00ba6e5223d381daaff184f4f9c65b93d343
Mariusz Sikora <msikora87@gmail.com>
2016-10-04 20:23:39 +0200

    Empty GpioAvr class

--- Gpio_driver.cpp
+++ Gpio_driver.cpp
@@ -1,5 +1,22 @@
 
-namespace Gpio_driver {
+#include "Gpio_driver.h"
 
+namespace gpio_driver {
 
-} /*namespace Gpio_driver*/
+void GpioAvr::Set(void) {
+
+}
+
+void GpioAvr::Clear(void) {
+
+}
+
+void GpioAvr::Toggle(void) {
+
+}
+
+uint8_t GpioAvr::GetState(void) {
+    return 0;
+}
+
+} /*namespace gpio_driver*/
